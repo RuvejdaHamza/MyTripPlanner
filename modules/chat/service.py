61b5e2e385ee53da7d3224248f657d5ad3be71fd
sarak1234 <sarakycyku123@gmail.com>
2026-05-16 10:23:13 +0200

    ndryshime en requimrments edhe ne service

--- modules/chat/service.py
+++ modules/chat/service.py
@@ -1,1 +1,20 @@
 """Chat service logic."""
+
+import os
+
+from dotenv import load_dotenv
+from google import genai
+from google.genai.types import HttpOptions
+
+
+load_dotenv()
+
+PROJECT_ID = os.getenv("GOOGLE_CLOUD_PROJECT")
+LOCATION = os.getenv("GOOGLE_CLOUD_LOCATION")
+
+client = genai.Client(
+    vertexai=True,
+    project=PROJECT_ID,
+    location=LOCATION,
+    http_options=HttpOptions(api_version="v1"),
+)
